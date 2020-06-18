6856b43406700f7acc59ec254d44ca2b93fd6cf3
SoySoy4444 <sshoinkyuraa.08.04@hotmail.co.jp>
2020-06-18 23:32:23 +0900

    Add check()

--- game.py
+++ game.py
@@ -33,6 +33,32 @@ class Game:
             self.squaresRemaining -= 1
             for row in self.board:
                 print(row)
+            self.won = self.check()  # False, "O" or "X"
+        print(f"{self.won} won the game!")
+
+    def check(self):
+        # Check for horizontal row
+        for row in self.board:
+            if row[0] != "-" and all(coordinate == row[0] for coordinate in row):
+                return row[0]  # return "O" or "X"
+
+        # Check for vertical column
+        for i in range(self.GRID_SIZE):  # for each column number
+            cols = [row[i] for row in self.board]
+            if cols[0] != "-" and all(coordinate == cols[0] for coordinate in cols):
+                return cols[0]  # return "O" or "X"
+
+        # check from top left to bottom right diagonal
+        if self.board[0][0] != "-" and all(self.board[i][i] == self.board[0][0] for i in range(self.GRID_SIZE)):
+            return self.board[0][0]  # return "O" or "X"
+
+        # check from bottom left to top right diagonal
+        if(self.board[self.GRID_SIZE-1][0] != "-" and
+                all(self.board[self.GRID_SIZE-i-1][i] == self.board[self.GRID_SIZE-1][0] for i in range(self.GRID_SIZE))):
+            return self.board[self.GRID_SIZE-1][0]  # return "O" or "X"
+
+        # Otherwise, nobody has won yet
+        return False
 
 
 g = Game("O", 3)
